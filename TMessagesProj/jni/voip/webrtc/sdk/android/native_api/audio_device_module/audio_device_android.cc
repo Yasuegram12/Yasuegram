c586d16eee6d149b26cddb652f0f8be0d69b7b41
Yasuegram12 <yswallh19@gmail.com>
2026-07-18 17:39:42 +0300

    Fix AAudio constructor mismatch

--- TMessagesProj/jni/voip/webrtc/sdk/android/native_api/audio_device_module/audio_device_android.cc
+++ TMessagesProj/jni/voip/webrtc/sdk/android/native_api/audio_device_module/audio_device_android.cc
@@ -70,8 +70,8 @@ rtc::scoped_refptr<AudioDeviceModule> CreateAAudioAudioDeviceModule(
       AudioDeviceModule::kAndroidAAudioAudio, false /* use_stereo_input */,
       false /* use_stereo_output */,
       jni::kLowLatencyModeDelayEstimateInMilliseconds,
-      std::make_unique<AAudioRecorder>(input_parameters),
-      std::make_unique<AAudioPlayer>(output_parameters));
+      std::make_unique<AAudioRecorder>(audio_manager),
+      std::make_unique<AAudioPlayer>(audio_manager));
 }
 
 rtc::scoped_refptr<AudioDeviceModule>
@@ -96,7 +96,7 @@ CreateJavaInputAndAAudioOutputAudioDeviceModule(JNIEnv* env,
       AudioDeviceModule::kAndroidJavaInputAndOpenSLESOutputAudio,
       false /* use_stereo_input */, false /* use_stereo_output */,
       jni::kLowLatencyModeDelayEstimateInMilliseconds, std::move(audio_input),
-      std::make_unique<AAudioPlayer>(output_parameters));
+      std::make_unique<AAudioPlayer>(audio_manager));
 }
 #endif
 
